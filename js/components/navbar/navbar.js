9b97cf9d99859c70f7825f2d4833dca06b0a3c02
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-06 21:18:15 +0100

    basic navbar component

---
+++ js/components/navbar/navbar.js
@@ -0,0 +1,8 @@
+define(['knockout', 'text!./navbar.html'], function(ko, templateHtml) {
+    return {
+        viewModel: function(params) {
+            var self = this;
+        },
+        template: templateHtml
+    };
+});
