84098df1fafd3fb5d049a71bd23ac1bbfb9a8b12
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-06 20:13:26 +0100

    observable array

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ define(['knockout'], function(ko) {
     var ViewModel = function() {
         var self = this;
         self.name = ko.observable();
+
+        self.countries = ko.observableArray([
+            { id: '0', name: 'Germany' },
+            { id: '1', name: 'Austria' },
+        ]);
     }
 
     return ViewModel;
