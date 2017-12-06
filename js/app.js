679650d99247989b8c4452cd2b775209db73f3e6
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-06 22:01:48 +0100

    wiring navigation to pages

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ define(['knockout'], function(ko) {
     var ViewModel = function() {
         var self = this;
         self.toggled = ko.observable(false);
+        self.currentPage = ko.observable('home');
     }
 
     return ViewModel;
