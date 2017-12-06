371d40b60f6a793bbd63790b7833e920f7890b7b
Jan Illetschko <jan.illetschko@3kraft.com>
2017-12-06 22:42:03 +0100

    page navigation with router

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-define(['knockout'], function(ko) {
+define(['knockout', 'router', 'bootstrap'], function(ko, router) {
 
     var ViewModel = function() {
         var self = this;
         self.toggled = ko.observable(false);
-        self.currentPage = ko.observable('home');
+        self.pages = ko.observableArray();
+        router.Router.initialized.then(function(resolve, rejected) {
+            self.pages(router.Router.head.routes);
+        });
     }
 
     return ViewModel;
--- js/components/navbar/navbar.js
+++ js/components/navbar/navbar.js
@@ -3,11 +3,7 @@ define(['knockout', 'text!./navbar.html'], function(ko, templateHtml) {
         viewModel: function(params) {
             var self = this;
             self.title = params.title;
-            self.currentPage = params.currentPage;
             self.pages = params.pages;
-            self.switchPage = function(page) {
-                self.currentPage(page);
-            }
         },
         template: templateHtml
     };
